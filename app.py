99cd3c4a15319556905ce26d93d87d788bfe181d
gunsnroses1000p-star <gunsnroses1000p@gmail.com>
2026-06-13 15:43:33 -0400

    Update API URL for Hugging Face inference

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 HF_TOKEN = os.getenv("HF_TOKEN")
 
-API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+API_URL = "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0"
 headers = {
     "Authorization": f"Bearer {HF_TOKEN}"
 }

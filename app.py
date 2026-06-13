46ba9acfbae8889e38d2efd409aabf322dec9a68
gunsnroses1000p-star <gunsnroses1000p@gmail.com>
2026-06-13 15:41:11 -0400

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import requests
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-
 headers = {
     "Authorization": f"Bearer {HF_TOKEN}"
 }

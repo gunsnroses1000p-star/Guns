d581fd6df427beb183580a36e78c66816603f76d
gunsnroses1000p-star <gunsnroses1000p@gmail.com>
2026-06-14 15:06:47 -0400

    Fix string formatting for image URLs

--- nightclub_ai.py
+++ nightclub_ai.py
@@ -9,8 +9,8 @@ if not FAL_KEY:
 image1_path = "in_painting_1781014281355.jpg"
 image2_path = "Screenshot_20260614_025608_AIReel.jpg"
 
-image1_url = https://i.postimg.cc/HWP9rhBw/Screenshot-20260614-145349-Pinterest.jpg
-image2_url = https://i.postimg.cc/pdZDRGmB/output.jpg
+image1_url = "https://i.postimg.cc/HWP9rhBw/Screenshot-20260614-145349-Pinterest.jpg"
+image2_url = "https://i.postimg.cc/pdZDRGmB/output.jpg"
 
 print("Image 1 URL:", image1_url)
 print("Image 2 URL:", image2_url)
